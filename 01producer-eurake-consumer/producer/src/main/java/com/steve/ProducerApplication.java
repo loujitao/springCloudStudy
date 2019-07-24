3b5e7ff795676998119b907f0b371ee853eb9aba
stevetao <31644511+loujitao@users.noreply.github.com>
2019-07-24 22:55:31 +0800

    idea里面测试git

--- 01producer-eurake-consumer/producer/src/main/java/com/steve/ProducerApplication.java
+++ 01producer-eurake-consumer/producer/src/main/java/com/steve/ProducerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
+		//test  ss
+				SpringApplication.run(ProducerApplication.class, args);
 	}
 }
