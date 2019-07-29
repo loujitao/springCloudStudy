57b03a77867ec44606dbb1d2d7258dd7bd463c1a
loujitao <995268272@qq.com>
2019-07-29 16:06:43 +0800

    eureka测试代码

---
+++ 03.eurakeTest/03eurakeCustomer/src/main/java/com/steve/CustomerApplication.java
@@ -0,0 +1,25 @@
+package com.steve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+//此注解代表它是eureka的客户端
+@EnableDiscoveryClient
+public class CustomerApplication {
+
+    @Bean
+    //负载均衡注解，调用参数里的URL选项写为服务名
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class,args);
+    }
+}
