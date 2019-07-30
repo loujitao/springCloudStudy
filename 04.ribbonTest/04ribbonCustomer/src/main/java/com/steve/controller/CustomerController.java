86e1d74f02ed2177cd3249e3521e065fb8c347f6
loujitao <995268272@qq.com>
2019-07-30 11:37:23 +0800

    ribbon测试代码

--- 04.ribbonTest/04ribbonCustomer/src/main/java/com/steve/controller/CustomerController.java
+++ 04.ribbonTest/04ribbonCustomer/src/main/java/com/steve/controller/CustomerController.java
@@ -1,4 +1,4 @@
-package com.steve.com.steve;
+package com.steve.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +12,8 @@ public class CustomerController {
     @Autowired
     private RestTemplate restTemplate;
 
-
     @GetMapping("/callHello")
     public String callHello(){
-        //通过硬编码调用服务提供者的接口
-//        return restTemplate.getForObject("http://localhost:8080/person/hello",String.class);
-        //通过服务名调用接口
-        return restTemplate.getForObject("http://03eureka-producer/person/hello",String.class);
+        return restTemplate.getForObject("http://04ribbon-producer/person/hello",String.class);
     }
 }
